51970e651939f3e817b3f30d73c124b0a1cc5692
ChungMan Lim <lleoncm@gmail.com>
2021-01-27 12:49:19 +0900

    Lotto Generator_4

--- Game_Lotto.cpp
+++ Game_Lotto.cpp
@@ -8,6 +8,130 @@
 
 using namespace std;
 
+void birthGenerator(int num_0, int num_1, int num_2, int num_3, int num_4, int num_5, int num_6, int num_7)
+{
+    // 숫자 생성
+    vector<int> checkVec;
+    checkVec.clear();
+
+    // 80%
+    checkVec.push_back(num_5 * 10 + num_4);  //0
+    checkVec.push_back(num_3 * 10 + num_2);  //1
+    checkVec.push_back(num_1 * 10 + num_0);  //2
+
+    // 50%
+    checkVec.push_back(num_7 * 10 + num_6);   //3
+    checkVec.push_back(num_4 * 10 + num_5);  //4
+    checkVec.push_back(num_2 * 10 + num_3);  //5
+    checkVec.push_back(num_0 * 10 + num_1);  //6
+    checkVec.push_back(num_5 + num_4);   //7
+    checkVec.push_back(num_3 + num_2);   //8
+    checkVec.push_back(num_1 + num_0);   //9
+    checkVec.push_back(checkVec[1] + checkVec[2]); //10
+
+    // 20%
+    checkVec.push_back(num_7 + num_6 + num_5 + num_4 + num_3 + num_2 + num_1 + num_0);   //11
+    checkVec.push_back(num_5 + num_4 + num_3 + num_2 + num_1 + num_0);   //12
+    checkVec.push_back(num_7 + num_6 + num_5 + num_4);   //13
+    checkVec.push_back(num_3 + num_2 + num_1 + num_0);   //14
+    checkVec.push_back(num_2 + num_0);   //15
+    checkVec.push_back(num_2 * num_0);   //16
+    checkVec.push_back(checkVec[1] * checkVec[2]);   //17
+
+    // 사용 가능한 숫자 & 중복 값 체크
+    for (int i = 0; i < checkVec.size(); i++)
+    {
+        if (checkVec[i] > 45)
+        {
+            checkVec[i] = 0;
+        }
+
+        for (int j = 0; j < i; j++)
+        {
+            if (checkVec[j] == checkVec[i])
+            {
+                checkVec[i] = 0;
+            }
+        }
+        //cout << checkVec[i] << endl;
+    }
+    //cout << endl;
+
+    random_device rd;
+    mt19937 gen(rd());
+
+    vector<int> realVec;
+
+    if ((gen() % 10) < 8 && checkVec[0] != 0)
+        realVec.push_back(checkVec[0]);
+    if ((gen() % 10) < 8 && checkVec[1] != 0)
+        realVec.push_back(checkVec[1]);
+    if ((gen() % 10) < 8 && checkVec[2] != 0)
+        realVec.push_back(checkVec[2]);
+
+    if ((gen() % 10) < 5 && checkVec[3] != 0)
+        realVec.push_back(checkVec[3]);
+    if ((gen() % 10) < 5 && checkVec[4] != 0)
+        realVec.push_back(checkVec[4]);
+    if ((gen() % 10) < 5 && checkVec[5] != 0)
+        realVec.push_back(checkVec[5]);
+    if ((gen() % 10) < 5 && checkVec[6] != 0)
+        realVec.push_back(checkVec[6]);
+    if ((gen() % 10) < 5 && checkVec[7] != 0)
+        realVec.push_back(checkVec[7]);
+    if ((gen() % 10) < 5 && checkVec[8] != 0)
+        realVec.push_back(checkVec[8]);
+    if ((gen() % 10) < 5 && checkVec[9] != 0)
+        realVec.push_back(checkVec[9]);
+    if ((gen() % 10) < 5 && checkVec[10] != 0)
+        realVec.push_back(checkVec[10]);
+
+    if ((gen() % 10) < 2 && checkVec[11] != 0)
+        realVec.push_back(checkVec[11]);
+    if ((gen() % 10) < 2 && checkVec[12] != 0)
+        realVec.push_back(checkVec[12]);
+    if ((gen() % 10) < 2 && checkVec[13] != 0)
+        realVec.push_back(checkVec[13]);
+    if ((gen() % 10) < 2 && checkVec[14] != 0)
+        realVec.push_back(checkVec[14]);
+    if ((gen() % 10) < 2 && checkVec[15] != 0)
+        realVec.push_back(checkVec[15]);
+    if ((gen() % 10) < 2 && checkVec[16] != 0)
+        realVec.push_back(checkVec[16]);
+    if ((gen() % 10) < 2 && checkVec[17] != 0)
+        realVec.push_back(checkVec[17]);
+
+    if (realVec.size() > 6) // 로또 번호가 6개 이상 안나오도록 조정
+    {
+        realVec.erase(realVec.begin() + 6, realVec.end());
+    }
+    else // 로또 번호가 6개가 되도록 조정
+    {
+        for (int i = realVec.size(); i < 6; i++)
+        {
+            int temp = gen() % 45 + 1;
+            realVec.push_back(temp);
+
+            for (int j = 0; j < i; j++)
+            {
+                if (realVec[j] == temp)
+                {
+                    realVec.erase(realVec.begin() + i);
+                    i--;
+                }
+            }
+        }
+    }
+ 
+    sort(realVec.begin(), realVec.end());
+
+    for (int n = 0; n < realVec.size(); n++)
+    {
+        cout << realVec[n] << " ";
+    }
+    cout << endl;
+}
+
 void randomGenerator(int startNum = 0, int num_0 = 0, int num_1 = 0, int num_2 = 0, int num_3 = 0, int num_4 = 0, int num_5 = 0)
 {
     int lotto[6];
@@ -51,9 +175,9 @@ public:
     {
         cin.ignore();
         cout << "\n";
-        cout << "=================" << endl;
+        cout << "=============================" << endl;
         cout << "1. 랜덤 번호 생성" << endl;
-        cout << "=================" << endl;
+        cout << "=============================" << endl;
         cout << "\n\n\n";
         cout << "로또 번호를 몇 개 생성할까요? 원하시는 개수를 다음과 같이 입력해주세요." << endl;
         cout << "(1개를 생성하고 싶으시면 '1' 입력 후 엔터, 5개를 생성하고 싶으시면 '5' 입력 후 엔터)" << endl;
@@ -103,9 +227,9 @@ public:
     {
         cin.ignore();
         cout << "\n";
-        cout << "=================" << endl;
+        cout << "=============================" << endl;
         cout << "2. 특정 숫자 포함 생성" << endl;
-        cout << "=================" << endl;
+        cout << "=============================" << endl;
         cout << "\n\n\n";
         cout << "어떤 숫자를 포함할까요? 숫자를 다음과 같이 입력해주세요.(1 ~ 5개만!)" << endl;
         cout << "(13, 24를 포함하고 싶으시면 '13 24' 입력 후 엔터(space로 띄어쓰기 해주세요.))" << endl;
@@ -119,6 +243,7 @@ public:
         stringstream orgNum(orgString);
         int newNum;
         vector<int> numVec;
+        numVec.clear();
         int SIZE;
 
         cout << "입력하신 숫자는 ";
@@ -217,6 +342,118 @@ public:
     }
 };
 
+class menu_3
+{
+public:
+    menu_3()
+    {
+        cin.ignore();
+        cout << "\n";
+        cout << "=============================" << endl;
+        cout << "3. 생년월일 연관 번호 생성" << endl;
+        cout << "=============================" << endl;
+        cout << "\n\n\n";
+        cout << "생년월일을 다음과 같이 입력해주세요." << endl;
+        cout << "(생년월일이 1994년 2월 11일인 경우 '19940211', 2002년 11월 3일인경우 '20021103'으로 입력)" << endl;
+        cout << "\n";
+
+        int orgInt;
+        cin >> orgInt;
+        cout << "\n";
+
+        vector<int> numVec;
+        numVec.clear();
+
+        if (orgInt < 10000000 || orgInt >= 100000000)
+        {
+            cout << "잘 못 입력하셨습니다. 생년월일을 다시 입력해주세요." << endl;
+            Sleep(3000);
+            system("cls");
+            menu_3();
+        }
+        else
+        {
+            while (orgInt > 0)
+            {
+                numVec.push_back(orgInt % 10);
+                orgInt /= 10;
+            }
+
+            if ((numVec[3] * 10 + numVec[2] > 0) && (numVec[3] * 10 + numVec[2] <= 12)
+                && (numVec[1] * 10 + numVec[0] > 0) && (numVec[1] * 10 + numVec[0] <= 31))
+            {
+                cout << "입력하신 생년월일은 ";
+
+                for (int i = 7; i >= 4; i--)
+                {
+                    cout << numVec[i];
+                }
+                cout << "년 ";
+
+                for (int i = 3; i >= 2; i--)
+                {
+                    cout << numVec[i];
+                }
+                cout << "월 ";
+
+                for (int i = 1; i >= 0; i--)
+                {
+                    cout << numVec[i];
+                }
+                cout << "일 입니다." << endl;
+                cout << "입력하신 생년월일을 기반으로 로또 번호를 생성하겠습니다." << endl;
+            }
+            else
+            {
+                cout << "잘 못 입력하셨습니다. 생년월일을 다시 입력해주세요." << endl;
+                Sleep(3000);
+                system("cls");
+                menu_3();
+            }
+        }
+
+        cout << "\n";
+        cout << "로또 번호를 몇 개 생성할까요? 원하시는 개수를 다음과 같이 입력해주세요." << endl;
+        cout << "(1개를 생성하고 싶으시면 '1' 입력 후 엔터, 5개를 생성하고 싶으시면 '5' 입력 후 엔터)" << endl;
+        cout << "\n";
+        //cout << "※ 메인 메뉴로 돌아가기를 원하시면 '0'을 입력해주세요." << endl;
+
+        int num;
+        cin >> num;
+
+        try
+        {
+            if (num <= 0) throw string("잘 못 입력하셨습니다. 0보다 큰 숫자를 입력하세요");
+            //else if (num == 0) mainMenu(); // 메인메뉴로 돌아가기
+            else
+            {
+                cout << "로또 번호 " << num << "개를 생성하겠습니다." << endl;
+                cout << "\n\n";
+                cout << "행운의 번호는?" << endl;
+                cout << "\n";
+                for (int n = 0; n < num; n++)
+                {
+                    cout << n + 1 << "번 번호 :\t";
+                    birthGenerator(numVec[0], numVec[1], numVec[2], numVec[3], numVec[4], numVec[5], numVec[6], numVec[7]);
+                }
+                cout << "\n\n";
+                cout << "-------------------------------------------------------------------------------" << endl;
+                cout << "\n";
+                cout << "♥1등 당첨을 기원합니다♥" << endl;
+                cout << "\n";
+            }
+        }
+        catch (string(error_message))
+        {
+            cout << error_message << endl;
+            Sleep(3000);
+            system("cls");
+            menu_3();
+        }
+
+    }
+};
+
 void choiceMenu()
 {
     int num;
@@ -240,6 +477,11 @@ void choiceMenu()
         {
             system("cls");
             menu_2();
+        }
+        else if (num == 3)
+        {
+            system("cls");
+            menu_3();
         }
     }
     catch (string(error_message))
@@ -269,7 +511,7 @@ public:
         cout << "\n";
         cout << "1. 랜덤 번호 생성" << endl;
         cout << "2. 특정 숫자 포함 생성" << endl;
-        cout << "3. 생일 관련 생성" << endl;
+        cout << "3. 생년월일 연관 번호 생성" << endl;
         cout << "4. 역대 1위 당첨번호 기반 생성" << endl;
         cout << "\n\n";
         cout << "-------------------------------------------------------------------------------" << endl;
@@ -287,6 +529,7 @@ public:
 int main()
 {
     mainMenu();
+    //menu_3();
 
     return 0;
 }
